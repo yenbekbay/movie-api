17d570fcd02dc896574e98027481f13332750594
Ayan Yenbekbay <ayan.yenb@gmail.com>
2016-11-09 15:19:05 +0600

    test(kinopoisk/getGeneralInfo): snapshot object model instead of values

--- src/kinopoisk/__tests__/getGeneralInfo-test.js
+++ src/kinopoisk/__tests__/getGeneralInfo-test.js
@@ -1,11 +1,29 @@
 /* @flow */
 
+import R from 'ramda';
+
 import getGeneralInfo from '../getGeneralInfo';
 
 const kpId = 714888; // Star Wars: The Force Awakens
 
+const childTypesForArray = R.memoize(R.pipe(R.map(R.type), R.uniq));
+const modelFromObject = (obj: ?Object) => R.map(
+  R.cond([
+    [
+      R.allPass([
+        R.pipe(R.type, R.equals('Array')),
+        R.pipe(childTypesForArray, R.length, R.equals(1)),
+      ]),
+      (val: Array<any>) => `Array<${R.pipe(childTypesForArray, R.head)(val)}>`,
+    ],
+    [R.pipe(R.type, R.contains(R.__, ['Object', 'Array'])), modelFromObject],
+    [R.T, R.type],
+  ]),
+  obj,
+);
+
 describe('getGeneralInfo', () => {
   it('fetches general movie info from kinopoisk for a given id', async () => {
-    expect(await getGeneralInfo(kpId)).toMatchSnapshot();
+    expect(modelFromObject(await getGeneralInfo(kpId))).toMatchSnapshot();
   });
 });

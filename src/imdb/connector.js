e664a619e8782bcf546696d2a159c0e8092bd62c
Ayan Yenbekbay <ayan.yenb@gmail.com>
2016-11-22 17:06:20 +0600

    feat(imdb): add reasonable throttling to rating requests

--- src/imdb/connector.js
+++ src/imdb/connector.js
@@ -1,6 +1,7 @@
 /* @flow */
 
 import DataLoader from 'dataloader';
+import PromiseThrottle from 'promise-throttle';
 import rp from 'request-promise-native';
 
 import { userAgent } from '../utils';
@@ -26,9 +27,14 @@ class ImdbConnector {
     });
   }
 
+  _ratingsThrottleQueue = new PromiseThrottle({
+    requestsPerSecond: 10,
+    promiseImplementation: Promise,
+  });
+
   ratingsLoader: { load: (imdbId: string) => Promise<any> } = new DataLoader(
-    (imdbIds: Array<string>) => Promise.all(
-      imdbIds.map((imdbId: string) => this._ratingsRp({
+    (imdbIds: Array<string>) => this._ratingsThrottleQueue.addAll(
+      imdbIds.map((imdbId: string) => () => this._ratingsRp({
         // eslint-disable-next-line max-len
         uri: `http://p.media-imdb.com/static-content/documents/v1/title/${imdbId}/ratings%3Fjsonp=imdb.rating.run:imdb.api.title.ratings/data.json`,
       })),
